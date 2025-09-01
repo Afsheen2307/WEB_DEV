7aa3ee4a3c134cfdac02027d8ce8521620f46053
AFSHEEN <91799904+Afsheen2307@users.noreply.github.com>
2025-09-02 00:55:15 +0530

    Add files via upload

---
+++ JS5_assignment/assign3.js
@@ -0,0 +1,10 @@
+let Person={
+    name:"Sam",
+    age:32,
+    city:"Paris"
+}
+console.log(Person);
+Person.city="New York";
+console.log(Person);
+Person.country="United States";
+console.log(Person);
