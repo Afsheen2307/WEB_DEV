d433b74d0f7a8d6c9591275d922e36197d3f34a9
AFSHEEN <91799904+Afsheen2307@users.noreply.github.com>
2025-09-05 22:33:47 +0530

    Delete JS6_i directory

--- JS6_i/prac2.js
+++
@@ -1,8 +0,0 @@
-function roll(){
-    console.log(Math.floor(Math.random()*6)+1);
-}
-
-roll();
-roll();
-roll();
-roll();
--- JS6_i/prac5.js
+++
@@ -1,8 +0,0 @@
-function sumofNo(n){
-    let sum=0;
-    for(let i=1;i<=n;i++){
-        sum+=i;
-    }
-    return sum;
-}
-console.log(sumofNo(100));
